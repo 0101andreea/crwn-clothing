a6d75a40a0f89d99d84951dde777483bd3cb9802
Andreea Neaga <andreea.neaga@intern.arobs.com>
2021-01-21 11:58:19 +0200

    implemented firebase utils, store authenticated users

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
 import SignInAndSignUpPage from './pages/sig-in-and-sign-up/sig-in-and-sign-up.component'
 
-import {auth} from './firebase/firebase.utils'
+import {auth, createUserProfileDocument} from './firebase/firebase.utils'
 
 class App extends React.Component {
   constructor(){
@@ -19,10 +19,30 @@ class App extends React.Component {
   }
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser:user })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      //if a user sign in, check if is signed in
+      //is is signed  and the is a document profile, in will get the userRef from profile document from the userAuth object
+      //if there is no document, will create a new object in firebase.utils and still get back the userRef
+      //set the local state with the snapShot id and snapShotdata
+      //if the current user sign out, will set the currentUser to null
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth)
+        //check if DB is updated at that ref with new data
 
-      console.log(user)
+        userRef.onSnapshot(snapShot => {
+          //get the data related to the user that is stored to our DB
+         this.setState({
+           currentUser: {
+             id: snapShot.id,
+             ...snapShot.data()
+           }
+         })
+         console.log(this.state)
+        })
+     
+      }
+      this.setState({currentUser:userAuth})
+    
     })
   }
 
